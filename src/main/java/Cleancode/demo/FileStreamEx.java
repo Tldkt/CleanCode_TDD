58426c4c9f643aaf8b230417b8dffaa554574787
Tldkt <spponing.Tldkt@gmail.com>
2022-07-23 22:23:07 +0900

    Feat:make FileInputStream

---
+++ src/main/java/Cleancode/demo/FileStreamEx.java
@@ -0,0 +1,19 @@
+/*package Cleancode.demo;
+
+import java.io.FileInputStream;
+
+public class FileStreamEx {
+    public static void main(String[] args) {
+        try{
+            FileInputStream fileInput = new FileInputStream("HelloFileStream.txt");
+                    int i = 0;
+            while((i =fileInput.read()) != -1){
+                System.out.print((char) i);
+            }
+            fileInput.close();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }*/
+    }
+}
